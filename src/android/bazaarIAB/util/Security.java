befbac8b88e5e6ca0130abd62f3679f05e4bfa8f
mahdi71 <m.heydarzadeh.71@gmail.com>
2016-04-02 16:04:01 +0430

    Update Security.java

--- src/android/bazaarIAB/util/Security.java
+++ src/android/bazaarIAB/util/Security.java
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-package com.example.android.trivialdrivesample.util;
+package ir.adPlay.bazaarIAB.util;
 
 import android.text.TextUtils;
 import android.util.Log;
