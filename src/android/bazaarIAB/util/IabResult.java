e940f1f5e40e40dd1700e644c9b83d833cd52165
mahdi71 <m.heydarzadeh.71@gmail.com>
2016-04-02 16:03:36 +0430

    Update IabResult.java

--- src/android/bazaarIAB/util/IabResult.java
+++ src/android/bazaarIAB/util/IabResult.java
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-package com.example.android.trivialdrivesample.util;
+package ir.adPlay.bazaarIAB.util;
 
 /**
  * Represents the result of an in-app billing operation.
